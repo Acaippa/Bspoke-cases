aaf19d115f16b032a4cecc3019e444f38293f9f1
Nils <olivernils8@gmail.com>
2023-03-22 14:51:28 +0100

    [ADD] Added case 7 solution

---
+++ code/Solution #7.py
@@ -0,0 +1,13 @@
+def find_all_hobbyists(hobby, hobbies):
+    # Loop through hobbies and add every name whos hobby-list has the supplied hobby in it.
+    return [ person for person in hobbies.keys() if hobby in hobbies[person] ]
+
+if __name__ == "__main__":
+    hobbies = { 
+        "Steve": ['Fashion', 'Piano', 'Reading'],
+        "Patty": ['Drama', 'Magic', 'Pets'],
+        "Chad": ['Puzzles', 'Pets', 'Yoga'],
+        "Nils": ['Programming', 'biking', 'Yoga'],
+    }
+    
+print(find_all_hobbyists('Yoga', hobbies)); # -> ['Chad', 'Nils']
